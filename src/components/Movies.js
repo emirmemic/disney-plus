4ce4121ff38ad55858df3660d83b49c5460e6875
Emir Memic <emirmemic@192.168.1.93>
2022-01-10 22:27:09 +0100

    Details.js  & Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,8 +7,30 @@ function Movies() {
       <h4>Recommended for You</h4>
       <Content>
         <Wrap>
-          <img src="" />
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
         </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" />
+        </Wrap>
+        
       </Content>
     </Container>
   );
@@ -18,6 +40,36 @@ export default Movies;
 
 const Container = styled.div``;
 
-const Content = styled.div``;
+const Content = styled.div`
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(4, minmax(0,1fr));
+  
+`;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  border-radius: 10px;
+  cursor: pointer;
+  overflow: hidden;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+  rgb(0 0 0 / 73%) 0px 16px 10px -10px ;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+
+
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 50px -16px
+    rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
+
+ 
